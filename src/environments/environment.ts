9b0ae7663e71d5c74393d3fb6fbafda1ff563d73
Paul R. Carlson <carlsonpar@gmail.com>
2021-01-11 23:41:56 -0500

    updated variable data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDJOmY7hp6eKSjsh7HswliOsWGSxJmC3ug",
-    authDomain: "nyny-b38bd.firebaseapp.com",
-    databaseURL: "https://nyny-b38bd-default-rtdb.firebaseio.com",
-    projectId: "nyny-b38bd",
-    storageBucket: "nyny-b38bd.appspot.com",
-    messagingSenderId: "463588515768",
-    appId: "1:463588515768:web:23fc33f192491e52fb794d",
-    measurementId: "G-VD8JT9PHV9"
-  }
+  //place your firebase configuration here
 };
 
 /*
