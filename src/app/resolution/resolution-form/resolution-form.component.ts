f55587d813a56d463ba155158d7d7f07b4000b82
Paul R. Carlson <carlsonpar@gmail.com>
2021-01-13 09:05:46 -0500

    continuation of the form dialog to add a new resolution.

--- src/app/resolution/resolution-form/resolution-form.component.ts
+++ src/app/resolution/resolution-form/resolution-form.component.ts
@@ -1,20 +1,18 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Inject } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
-export interface DialogData {
-  title: string;
-  description: string;
-}
 
 @Component({
   selector: 'app-resolution-form',
   templateUrl: './resolution-form.component.html',
   styleUrls: ['./resolution-form.component.scss']
 })
-export class ResolutionFormComponent {
+export class ResolutionFormComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any) { }
+  ngOnInit() {
+
+  }
 
   
     
--- src/app/resolution/resolution-list/resolution-list.component.ts
+++ src/app/resolution/resolution-list/resolution-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { ResolutionService } from 'src/app/shared/services/resolution.service';
+import { ResolutionFormComponent } from '../resolution-form/resolution-form.component';
 
 @Component({
   selector: 'app-resolution-list',
@@ -10,17 +12,25 @@ import { ResolutionService } from 'src/app/shared/services/resolution.service';
 export class ResolutionListComponent implements OnInit {
   ResolutionList = null
   CategoryId
+  id
 
   constructor(
     private rs: ResolutionService,
-    private route: ActivatedRoute ) { }
+    private route: ActivatedRoute,
+    private dialog: MatDialog ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.rs.GetResolutions(id).subscribe(data=>{
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.rs.GetResolutions(this.id).subscribe(data=>{
       this.ResolutionList = data
     })
   }
-  
+   OpenNewDialog(){
+    console.log(this.id)
+    let dialogRef = this.dialog.open(ResolutionFormComponent,  {data: {id: this.id}})
+    dialogRef.afterClosed().subscribe(result=>{
+      console.log(result)
+    })
+  }
 
 }
